ae4b41861782beceec18d73a69cb489a63fc4572
Denzil Villarico <dev@denzilv.com>
2024-09-18 10:57:07 -0700

    Add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# allows us to use code from the pygame library
+import pygame
+
+def main():
+    print("Starting asteroids!")
+
+# ensures main() is only called when this file is run directly
+# won't run as an imported module and is the pythonic way
+if __name__ == "__main__":
+    main()
