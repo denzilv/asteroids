314c5ce6c5f8738acc85f6dd256cfb5fe0f3962a
Denzil Villarico <dev@denzilv.com>
2024-09-18 11:24:24 -0700

    Add game loop and .gitignore

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    # initialize pygame
+    pygame.init()
+
+    # GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            pygame.Surface.fill(screen, "black")
+            pygame.display.flip()
+
 # ensures main() is only called when this file is run directly
 # won't run as an imported module and is the pythonic way
 if __name__ == "__main__":
