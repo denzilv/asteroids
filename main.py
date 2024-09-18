2a6ea566634b78165ec9032dc56d3b2525b005ec
Denzil Villarico <dev@denzilv.com>
2024-09-18 11:06:56 -0700

    Add constants.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # allows us to use code from the pygame library
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
 
 # ensures main() is only called when this file is run directly
 # won't run as an imported module and is the pythonic way
